a1ea6405ac77fafca85c87ba75f329682d3657ce
Barbosa <barbosa.elivelton.dev@gmail.com>
2025-03-10 13:49:29 -0300

    feat: implement item addition with validation in shopping list

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const saveButton = document.getElementById("adicionar-item");
 const inputItem = document.getElementById("input-item")
+const listaDeCompras = document.getElementById("lista-de-compras")
+let contador = 0;
 
 saveButton.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(inputItem.value);
+    if (inputItem.value === "") {
+        alert("Por favor, digite o nome do item.");
+        return;
+    }
+    const itemLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = `checkbox-${contador++}`;
+    const itemText = document.createElement("p");
+    itemText.innerText = inputItem.value;
+
+    containerItemLista.appendChild(inputCheckbox);
+    containerItemLista.appendChild(itemText)
+
+    itemLista.appendChild(containerItemLista)
+    listaDeCompras.appendChild(itemLista)
 });
