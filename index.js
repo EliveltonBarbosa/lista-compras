c218b2775b00ee3c01e4d3adb80edc2e9baf3a4c
Barbosa <barbosa.elivelton.dev@gmail.com>
2025-03-11 08:56:47 -0300

    feat: add timestamp display for added items in shopping list

--- index.js
+++ index.js
@@ -23,4 +23,17 @@ saveButton.addEventListener("click", (event) => {
 
     itemLista.appendChild(containerItemLista)
     listaDeCompras.appendChild(itemLista)
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"})
+        .charAt(0)
+        .toUpperCase() + new Date().toLocaleDateString("pt-BR", {weekday: "long"})
+        .slice(1);
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {hour: "2-digit", minute: "2-digit"});
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    
+    const textoData = document.createElement("p");
+    textoData.classList.add("texto-data");
+    textoData.innerText = dataCompleta;
+    itemLista.appendChild(textoData);
 });
