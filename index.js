83b36304681a5446c8e7eb6a7a7c496a4da2d8fb
Barbosa <barbosa.elivelton.dev@gmail.com>
2025-03-12 07:31:13 -0300

    feat: move empty list check to separate module and update event handler to use it

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 import { criarItemLista } from "./scripts/criarItemLista.js";
+import verificarListaVazia from "./scripts/verificarListaVazia.js";
 
 const saveButton = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
 
 saveButton.addEventListener("click", (event) => {
     event.preventDefault();
     const itemLista = criarItemLista();
     listaDeCompras.appendChild(itemLista)
-    verificarListaVazia();
+    verificarListaVazia(listaDeCompras);
 });
 
-function verificarListaVazia() {
-    const itensDaLista = listaDeCompras.querySelectorAll("li");
-    if (itensDaLista.length === 0) {
-        mensagemListaVazia.style.display = "block"
-    } else {
-        mensagemListaVazia.style.display = "none"
-    }
-}
-
-verificarListaVazia();
+verificarListaVazia(listaDeCompras);
